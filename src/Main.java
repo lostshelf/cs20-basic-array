af4251f26cf415b8e090a1459b5a0303e2485052
Hadi Faraz <hadif2@educbe.ca>
2023-03-01 15:25:58 -0700

    Start question 6 implementation

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,16 @@ public class Main {
         // Question 4
         Integer[] intArray = new Integer[rand.nextInt(21) + 5];
         printArray(intArray);
+
+        // Question 6
+        // Create two arrays of 5 Strings (assign the Strings any text you want). Combine them into a new array
+        // (this is called "concatenation"). EXTRA: Create two String arrays with a random size from 4 to 8 elements.
+        // Concatenate these two arrays.
+        String[] stringArray1 = new String[rand.nextInt(5) + 4];
+        String[] stringArray2 = new String[rand.nextInt(5) + 4];
+
+        String[] combined = new String[stringArray1.length + stringArray2.length];
+        System.arraycopy(stringArray1, 0, combined, 0);
     }
 
     public static <T> void printArray(T[] arr) {
